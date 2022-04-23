5d20d2882264f2adf4be14511b5f5ad9f3185f28
Asuna-Yuki <rahulraj.sagg99@gmail.com>
2022-04-24 00:38:01 +0530

    User registration with password encryption

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+
+const User = require("../../models/User");
 
 // @route  POST api/users
 // @desc   Register user
@@ -15,13 +18,43 @@ router.post(
       "Please enter a password with 8 or more characters"
     ).isLength({ min: 8 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("users route");
+
+    const { name, email, password } = req.body;
+
+    try {
+      // See if the user exists
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists." }] });
+      }
+
+      user = new User({
+        name: name,
+        email: email,
+        password: password,
+      });
+      // encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      // Return jsonwebtoken
+
+      res.send("Users registered");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error.");
+    }
   }
 );
 
