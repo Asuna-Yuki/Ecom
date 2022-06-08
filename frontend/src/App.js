c74d2b5365322613be8bee438574c1c946ad82f4
Asuna-Yuki <rahulraj.sagg99@gmail.com>
2022-06-08 22:39:25 +0530

    connected all layout to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,30 @@
 import React, { Fragment } from "react";
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Switch,
+} from "react-router-dom";
 import Navbar from "./components/layouts/Navbar";
 import Home from "./components/layouts/Home";
-import Footer from "./components/Footer";
+import Footer from "./components/layouts/Footer";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Product from "./components/layouts/Product";
 
 const App = () => {
   return (
-    <Fragment>
+    <Router>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/register' element={<Register />} />
+        <Route exact path='/login' element={<Login />} />
+        <Route exact path='/product' element={<Product />} />
+      </Routes>
       <Footer />
-    </Fragment>
+    </Router>
   );
 };
 
