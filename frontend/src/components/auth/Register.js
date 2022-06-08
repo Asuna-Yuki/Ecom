32e4cbc67d5c74c6c3ee261cb7edf97b9be668a9
Asuna-Yuki <rahulraj.sagg99@gmail.com>
2022-06-08 22:37:42 +0530

    created register and login layout

---
+++ frontend/src/components/auth/Register.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+import Navbar from "../layouts/Navbar";
+import Footer from "../layouts/Footer";
+
+const Register = () => {
+  return <h1>Register</h1>;
+};
+
+export default Register;
