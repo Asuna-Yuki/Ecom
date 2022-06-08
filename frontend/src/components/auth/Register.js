6e01131c23df9fed4e5039bddfe3d390810a74a0
Asuna-Yuki <rahulraj.sagg99@gmail.com>
2022-06-08 23:42:50 +0530

    created login register and cart layouts

--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -3,7 +3,44 @@ import Navbar from "../layouts/Navbar";
 import Footer from "../layouts/Footer";
 
 const Register = () => {
-  return <h1>Register</h1>;
+  return (
+    <div className='main'>
+      <div className='register'>
+        <h1>Sign in</h1>
+        <hr></hr>
+        <form action=''>
+          <h4>Name</h4>
+          <input
+            className='register-input'
+            type='text'
+            placeholder='First Name'
+            required
+          ></input>
+          <h4>Phone Number</h4>
+          <input
+            className='register-input'
+            type='number'
+            placeholder='Phone Number'
+            required
+          ></input>
+          <h4>Email</h4>
+          <input
+            className='register-input'
+            type='text'
+            placeholder='Email'
+            required
+          ></input>
+          <h4>Password</h4>
+          <input
+            type='password'
+            className='register-input'
+            placeholder='Password'
+          ></input>
+          <button className='btn register-btn'>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
---
+++ frontend/src/components/layouts/Cart.js
@@ -0,0 +1,52 @@
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+
+const Cart = () => {
+  return (
+    <div className='main'>
+      <h1>shoping cart</h1>
+      <div className='cart'>
+        <div className='cart-card-img'>
+          <img className='image' src='images/Itsuki.jpg' alt=''></img>
+        </div>
+        <div className='cart-card-des'>
+          <h1>Product name</h1>
+          <p>Status</p>
+          <div className='cart-card-des-menu'>
+            <input type='number' className='quantity-input cart-input'></input>
+            <a href='#'>Remove</a>
+          </div>
+        </div>
+      </div>
+      <div className='cart'>
+        <div className='cart-card-img'>
+          <img className='image' src='images/Nino.png' alt=''></img>
+        </div>
+        <div className='cart-card-des'>
+          <h1>Product name</h1>
+          <p>Status</p>
+          <div className='cart-card-des-menu'>
+            <input type='number' className='quantity-input cart-input'></input>
+            <a href='#'>Remove</a>
+          </div>
+        </div>
+      </div>
+      <div className='cart'>
+        <div className='cart-card-img'>
+          <img className='image' src='images/Miku.jpg' alt=''></img>
+        </div>
+        <div className='cart-card-des'>
+          <h1>Product name</h1>
+          <p>Status</p>
+          <div className='cart-card-des-menu'>
+            <input type='number' className='quantity-input cart-input'></input>
+            <a href='#'>Remove</a>
+          </div>
+        </div>
+      </div>
+      <hr></hr>
+    </div>
+  );
+};
+
+export default Cart;
