2d8097cef8a0e08b9362c5eaa610d2392ecb6839
Asuna-Yuki <rahulraj.sagg99@gmail.com>
2022-06-09 22:45:34 +0530

    added onSubmit and onChange functions

--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -1,40 +1,74 @@
-import React, { Fragment } from "react";
-import Navbar from "../layouts/Navbar";
-import Footer from "../layouts/Footer";
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log("Passwords do not match.");
+    } else {
+      console.log(formData);
+    }
+  };
+
   return (
     <div className='main'>
+      <Link to='/'>
+        <button className='back-btn btn'>Go Back</button>
+      </Link>
       <div className='register'>
         <h1>Sign in</h1>
         <hr></hr>
-        <form action=''>
+        <form onSubmit={(e) => onSubmit(e)}>
           <h4>Name</h4>
           <input
             className='register-input'
             type='text'
-            placeholder='First Name'
+            placeholder='Name'
+            name='name'
+            value={name}
+            onChange={(e) => onChange(e)}
             required
-          ></input>
-          <h4>Phone Number</h4>
-          <input
-            className='register-input'
-            type='number'
-            placeholder='Phone Number'
-            required
-          ></input>
+          />
           <h4>Email</h4>
           <input
             className='register-input'
             type='text'
             placeholder='Email'
+            name='email'
+            value={email}
+            onChange={(e) => onChange(e)}
             required
-          ></input>
+          />
           <h4>Password</h4>
           <input
             type='password'
             className='register-input'
             placeholder='Password'
+            name='password'
+            value={password}
+            onChange={(e) => onChange(e)}
+            required
+          />
+          <h4>Confirm Password</h4>
+          <input
+            type='password'
+            className='register-input'
+            placeholder='Confirm Password'
+            name='password2'
+            value={password2}
+            onChange={(e) => onChange(e)}
           ></input>
           <button className='btn register-btn'>Submit</button>
         </form>
