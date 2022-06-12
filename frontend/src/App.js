7882c458c44c4722d1cac218a574dbb3d66f4fc9
Asuna-Yuki <rahulraj.sagg99@gmail.com>
2022-06-12 23:07:37 +0530

    connected alert layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,6 @@
-import React, { Fragment } from "react";
+import React from "react";
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Switch,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layouts/Navbar";
 import Home from "./components/layouts/Home";
 import Footer from "./components/layouts/Footer";
@@ -13,6 +8,7 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Product from "./components/layouts/Product";
 import Cart from "./components/layouts/Cart";
+import Alert from "./components/layouts/Alert";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -22,6 +18,7 @@ const App = () => {
     <Provider store={store}>
       <Router>
         <Navbar />
+        <Alert />
         <Routes>
           <Route exact path='/' element={<Home />} />
           <Route path='/register' element={<Register />} />
