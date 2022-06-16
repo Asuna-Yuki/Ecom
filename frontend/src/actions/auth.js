613f46835d753e3c64630c2dd1391cb349e64166
Asuna-Yuki <rahulraj.sagg99@gmail.com>
2022-06-16 23:22:14 +0530

    added login action and reducer

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -4,12 +4,13 @@ import {
   REGISTER_SUCCESS,
   USER_LOADED,
   AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
 } from "./types";
 import { setAlert } from "../actions/alert";
 import setAuthToken from "../utils/setAuthToken";
 
 // Load user
-
 export const loadUser = () => async (dispatch) => {
   if (localStorage.token) {
     setAuthToken(localStorage.token);
@@ -48,6 +49,8 @@ export const register =
         type: REGISTER_SUCCESS,
         payload: res.data,
       });
+
+      dispatch(loadUser());
     } catch (err) {
       const errors = err.response.data.errors;
 
@@ -59,3 +62,35 @@ export const register =
       });
     }
   };
+
+// Login User
+
+export const login = (email, password) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const res = await axios.post("/api/auth", body, config);
+
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data,
+    });
+
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg)));
+    }
+    dispatch({
+      type: LOGIN_FAIL,
+    });
+  }
+};
