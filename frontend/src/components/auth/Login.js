a8ce286a303c0856825ecb3259b38e32db63cd99
Asuna-Yuki <rahulraj.sagg99@gmail.com>
2022-06-16 23:22:41 +0530

    connected login action

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -1,7 +1,24 @@
-import React, { Fragment } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { login } from "../../actions/auth";
+import PropTypes from "prop-types";
 
-const Login = () => {
+const Login = ({ login }) => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    login(email, password);
+  };
+
+  const { email, password } = formData;
   return (
     <div className='main'>
       <Link to='/'>
@@ -10,26 +27,26 @@ const Login = () => {
       <div className='register'>
         <h1>Log in</h1>
         <hr></hr>
-        <form action=''>
-          <h4>Name</h4>
-          <input
-            className='register-input'
-            type='text'
-            placeholder='First Name'
-            required
-          ></input>
+        <form onSubmit={(e) => onSubmit(e)}>
           <h4>Email</h4>
           <input
             className='register-input'
             type='text'
             placeholder='Email'
+            name='email'
+            value={email}
+            onChange={(e) => onChange(e)}
             required
           ></input>
           <h4>Password</h4>
           <input
-            type='password'
             className='register-input'
+            type='password'
             placeholder='Password'
+            name='password'
+            value={password}
+            onChange={(e) => onChange(e)}
+            minLength='8'
           ></input>
           <button className='btn register-btn'>Log in</button>
         </form>
@@ -40,5 +57,7 @@ const Login = () => {
     </div>
   );
 };
-
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+};
+export default connect(null, { login })(Login);
