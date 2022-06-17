da79a0b12b1ca58b524a5663a2daa1670eb4a372
Asuna-Yuki <rahulraj.sagg99@gmail.com>
2022-06-17 20:44:07 +0530

    added logout action and reducer

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -6,6 +6,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  LOGOUT,
 } from "./types";
 import { setAlert } from "../actions/alert";
 import setAuthToken from "../utils/setAuthToken";
@@ -93,4 +94,11 @@ export const login = (email, password) => async (dispatch) => {
       type: LOGIN_FAIL,
     });
   }
+};
+
+// Logout user
+export const logout = () => (dispatch) => {
+  dispatch({
+    type: LOGOUT,
+  });
 };
--- frontend/src/reducers/auth.js
+++ frontend/src/reducers/auth.js
@@ -5,6 +5,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  LOGOUT,
 } from "../actions/types";
 
 const initialState = {
@@ -45,6 +46,17 @@ export default function (state = initialState, action) {
         isAuthenticated: false,
         loading: false,
       };
+
+    case LOGOUT:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        user: null,
+      };
+
     default:
       return state;
   }
