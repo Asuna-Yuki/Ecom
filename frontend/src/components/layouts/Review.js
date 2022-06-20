48db80096eb80bcf73238bf949b044bc37176a69
Asuna-Yuki <rahulraj.sagg99@gmail.com>
2022-06-20 23:28:36 +0530

    created Review component

---
+++ frontend/src/components/layouts/Review.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Review = () => {
+  return (
+    <div className='review'>
+      <p>Name</p>
+      <p>date - 32/13/4020</p>
+      <p>fiwehf hfeuiwhf ohfweohf oiahf oewih</p>
+      <br></br>
+      <hr></hr>
+    </div>
+  );
+};
+
+export default Review;
