129634f97cb9803eadabf3471e972779fb4b648a
Asuna-Yuki <rahulraj.sagg99@gmail.com>
2022-07-04 00:46:46 +0530

    added payment selection method

--- frontend/src/components/auth/Checkout.js
+++ frontend/src/components/auth/Checkout.js
@@ -1,66 +1,71 @@
 import { connect } from "react-redux";
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
 const Checkout = ({
   auth: { isAuthenticated, loading },
-  cart: { cartItems, shippingAddress, paymentMethod },
+  cart: { cartItems, shippingAddress, paymentMethod, cartLoading },
 }) => {
   if (!loading && !isAuthenticated) {
     return <Navigate to='/' />;
   }
-  return (
-    <div className='main'>
-      <div className='row'>
-        <div className='product-column-2'>
-          <h1>SHIPPING</h1>
-          <p>
-            {shippingAddress.address},{shippingAddress.city},
-            {shippingAddress.state},{shippingAddress.pincode}
-          </p>
-          <hr />
-          <h1>PAYMENT METHOD</h1>
-          <p>{paymentMethod.data}</p>
-          <hr />
-          <h1>ORDEER ITEMS</h1>
-          <p>{cartItems[0].name}</p>
-        </div>
-        <div className='product-column-3'>
-          <div className='product-detail'>
-            <div className='product-detail-item'>
-              <div className='row'>
-                <h1>ORDER SUMMARY</h1>
+  if (!cartLoading) {
+    return (
+      <div className='main'>
+        <Link to='/payment'>
+          <button className='back-btn btn'>Go Back</button>
+        </Link>
+        <div className='row'>
+          <div className='product-column-2'>
+            <h1>SHIPPING</h1>
+            <p>
+              {shippingAddress.address},{shippingAddress.city},
+              {shippingAddress.state},{shippingAddress.pincode}
+            </p>
+            <hr />
+            <h1>PAYMENT METHOD</h1>
+            <p>{paymentMethod ? paymentMethod : `No method selected`}</p>
+            <hr />
+            <h1>ORDEER ITEMS</h1>
+            <p>{cartItems[0].name}</p>
+          </div>
+          <div className='product-column-3'>
+            <div className='product-detail'>
+              <div className='product-detail-item'>
+                <div className='row'>
+                  <h1>ORDER SUMMARY</h1>
+                </div>
               </div>
-            </div>
-            <div className='product-detail-item'>
-              <div className='row'>
-                <p>Items:</p>
-                <p>{cartItems.length}</p>
+              <div className='product-detail-item'>
+                <div className='row'>
+                  <p>Items:</p>
+                  <p>{cartItems.length}</p>
+                </div>
               </div>
-            </div>
-            <div className='product-detail-item'>
-              <div className='row'>
-                <p>Shipping:</p>
-                <p>100</p>
+              <div className='product-detail-item'>
+                <div className='row'>
+                  <p>Shipping:</p>
+                  <p>100</p>
+                </div>
               </div>
-            </div>
-            <div className='product-detail-item'>
-              <div className='row'>
-                <p>Total:</p>
-                <p>100</p>
+              <div className='product-detail-item'>
+                <div className='row'>
+                  <p>Total:</p>
+                  <p>100</p>
+                </div>
               </div>
-            </div>
-            <div className='product-detail-item'>
-              <a href='#!'>
-                <button className='add-to-cart btn'>Place Order</button>
-              </a>
+              <div className='product-detail-item'>
+                <a href='#!'>
+                  <button className='add-to-cart btn'>Place Order</button>
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 Checkout.propTypes = {
--- frontend/src/components/auth/Payment.js
+++ frontend/src/components/auth/Payment.js
@@ -1,15 +1,16 @@
 import { connect } from "react-redux";
-import React from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import { paymentMethod } from "../../actions/cart";
 
 const Payment = ({ auth: { isAuthenticated, loading }, paymentMethod }) => {
+  const [method, setMethod] = useState("paypal");
   let navigate = useNavigate();
 
   const onSubmit = (e) => {
     e.preventDefault();
-    paymentMethod({ data: "paypal" });
+    paymentMethod(method);
     navigate("/checkout");
   };
 
@@ -19,19 +20,37 @@ const Payment = ({ auth: { isAuthenticated, loading }, paymentMethod }) => {
 
   return (
     <div className='main'>
+      <Link to='/address'>
+        <button className='back-btn btn'>Go Back</button>
+      </Link>
       <h1>PAYMENT METHOD</h1>
       <br />
       <br />
       <h2>SELECT PAYMENT METHOD</h2>
       <form onSubmit={(e) => onSubmit(e)}>
         <h3>Paypal</h3>
-        <input type='radio' name='paypal' value='paypal' />
+        <input
+          type='radio'
+          name='paymentMethod'
+          value='paypal'
+          onChange={(e) => setMethod(e.target.value)}
+        />
         <br />
         <h3>Google Pay</h3>
-        <input type='radio' name='googlePay' value='googlePay' />
+        <input
+          type='radio'
+          name='paymentMethod'
+          value='googlePay'
+          onChange={(e) => setMethod(e.target.value)}
+        />
         <br />
         <h3>Cash on Delivery</h3>
-        <input type='radio' name='cod' value='cod' />
+        <input
+          type='radio'
+          name='paymentMethod'
+          value='cod'
+          onChange={(e) => setMethod(e.target.value)}
+        />
         <br />
         <button className='btn register-btn'>Proceed to Checkout</button>
       </form>
