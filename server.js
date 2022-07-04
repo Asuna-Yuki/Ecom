77780ab5984bdb89c74e72145d8d6f2cba707e96
Asuna-Yuki <rahulraj.sagg99@gmail.com>
2022-07-04 21:49:44 +0530

    added checkout to index

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/review", require("./routes/api/review"));
 app.use("/api/product", require("./routes/api/product"));
+app.use("/api/checkout", require("./routes/api/checkout"));
 
 const PORT = process.env.PORT || 5000;
 
