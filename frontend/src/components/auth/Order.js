b712e018939cb909b680c1827ea7cc2eaa478136
Asuna-Yuki <rahulraj.sagg99@gmail.com>
2022-07-08 20:30:49 +0530

    populated order component

--- frontend/src/components/auth/Order.js
+++ frontend/src/components/auth/Order.js
@@ -4,7 +4,11 @@ import { Link, Navigate, useParams } from "react-router-dom";
 import PropTypes from "prop-types";
 import { getOrderById } from "../../actions/order";
 
-const Order = ({ auth: { loading, isAuthenticated }, getOrderById, order }) => {
+const Order = ({
+  auth: { loading, isAuthenticated, user },
+  getOrderById,
+  order: { orderDetail, orderLoading },
+}) => {
   // GEt product by id
   useEffect(() => {
     getOrderById(orderId);
@@ -13,19 +17,31 @@ const Order = ({ auth: { loading, isAuthenticated }, getOrderById, order }) => {
   let params = useParams();
 
   const orderId = params.id;
-  console.log(order.orderDetail);
 
   if (!loading && !isAuthenticated) {
     return <Navigate to='/' />;
   }
-  return (
-    <div className='main'>
-      <Link to='/payment'>
-        <button className='back-btn btn'>Go Back</button>
-      </Link>
-      <h1>{orderId}</h1>
-    </div>
-  );
+  if (!loading) {
+    return (
+      <div className='main'>
+        <Link to='/'>
+          <button className='back-btn btn'>Go Back</button>
+        </Link>
+        <h1>{orderId}</h1>
+        <hr />
+        <h2>Name : {user.name}</h2>
+        <h3>Email : {user.email}</h3>
+        <hr />
+        <p>
+          Address :{" "}
+          {!orderLoading ? orderDetail.shippingAddress.address : `spinner`}
+        </p>
+        <hr />
+        <p>Payment : {!orderLoading ? orderDetail.paymentMethod : `spinner`}</p>
+        <h4>{!orderLoading ? orderDetail.orderItems[0].name : `spinner`}</h4>
+      </div>
+    );
+  }
 };
 
 Order.propTypes = {
