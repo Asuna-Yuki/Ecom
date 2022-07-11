7315eeb971ce6bfb46be59e309ad8638bc30da3b
Asuna-Yuki <rahulraj.sagg99@gmail.com>
2022-07-11 23:41:21 +0530

    added middleware and admin paths

--- routes/api/product.js
+++ routes/api/product.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+// Middleware
+const auth = require("../../middleware/auth");
+const admin = require("../../middleware/admin");
 // Models
 const Product = require("../../models/Product");
 const Rreview = require("../../models/Review");
@@ -7,7 +10,6 @@ const Rreview = require("../../models/Review");
 // @Route  GET api/product
 // @desc   getting all products
 // @access Public
-
 router.get("/", async (req, res) => {
   try {
     const products = await Product.find();
@@ -26,7 +28,6 @@ router.get("/", async (req, res) => {
 // @Route  GET api/product
 // @desc   getting one products
 // @access Public
-
 router.get("/:id", async (req, res) => {
   try {
     const product = await Product.findById(req.params.id); //req.params.id
@@ -41,5 +42,83 @@ router.get("/:id", async (req, res) => {
     res.status(500).send("Server error.--one product");
   }
 });
+
+// @Route  POST api/product
+// @desc   edit product
+// @access Private/Admin
+router.post("/:id", auth, admin, async (req, res) => {
+  const { name, image, price, description, quantity } = req.body;
+  try {
+    // see if the product exists
+    let product = await Product.findById(req.params.id);
+
+    if (product) {
+      product.name = name;
+      product.image = image;
+      product.price = price;
+      product.description = description;
+      product.quantity = quantity;
+    } else {
+      return res.status(404).json({ erors: [{ msg: "Product not found." }] });
+    }
+
+    await product.save();
+
+    res.json(product);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error.--product update.");
+  }
+});
+
+// @Route  POST api/product
+// @desc   create new product
+// @access Private/Admin
+
+router.post("/", auth, admin, async (req, res) => {
+  const { name, image, price, description, quantity } = req.body;
+  try {
+    // see if the product exists
+    let product = await Product.findOne({ name: name });
+
+    if (product) {
+      return res.status(400).json({ msg: "Product already exists." });
+    }
+
+    product = new Product({
+      name,
+      image,
+      price,
+      description,
+      quantity,
+    });
+
+    await product.save();
+
+    res.json(product);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error.--product create.");
+  }
+});
+
+// @Route  GET api/product
+// @desc   get product by id
+// @access Private/Admin
+
+// router.get("/:id", auth, admin, async (req, res) => {
+//   try {
+//     const product = await Product.findById(req.params.id); //req.params.id
+
+//     if (!product) {
+//       return res.status(404).send("Product not found.");
+//     }
+
+//     res.json(product);
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send("Server error.--one product");
+//   }
+// });
 
 module.exports = router;
