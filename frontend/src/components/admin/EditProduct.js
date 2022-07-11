5ba3cc536136ddf62e15a9c17f79c118fa6de3af
Asuna-Yuki <rahulraj.sagg99@gmail.com>
2022-07-11 23:40:56 +0530

    created editproduct component updated product info

---
+++ frontend/src/components/admin/EditProduct.js
@@ -0,0 +1,111 @@
+import React, { useEffect, useState } from "react";
+import { Link, Navigate, useParams } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { editProduct } from "../../actions/admin";
+
+const EditProduct = ({ admin, editProduct }) => {
+  const [formData, setFormData] = useState({
+    name: admin.productDetails.name,
+    price: admin.productDetails.price,
+    description: admin.productDetails.description,
+    quantity: admin.productDetails.quantity,
+    image: admin.productDetails.image,
+  });
+
+  const { name, price, quantity, description, image } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    editProduct(admin.productDetails._id, {
+      name,
+      price,
+      quantity,
+      description,
+      image,
+    });
+  };
+
+  if (
+    admin.productDetails && // 👈 null and undefined check
+    Object.keys(admin.productDetails).length === 0 &&
+    Object.getPrototypeOf(admin.productDetails) === Object.prototype
+  ) {
+    return <Navigate to='/admin/productlist' />;
+  }
+  return (
+    <div className='main'>
+      <Link to='/admin/productlist'>
+        <button className='back-btn btn'>Go Back</button>
+      </Link>
+      <h1>EDIT PRODUCT</h1>
+      <form onSubmit={(e) => onSubmit(e)}>
+        <h2>Name</h2>
+        <input
+          className='register-input'
+          type='text'
+          placeholder='Name'
+          name='name'
+          value={name}
+          onChange={(e) => onChange(e)}
+          required
+        />
+        <h2>Price</h2>
+        <input
+          className='register-input'
+          type='number'
+          placeholder='Price'
+          name='price'
+          value={price}
+          onChange={(e) => onChange(e)}
+          required
+        />
+        <h2>Quantity</h2>
+        <input
+          className='register-input'
+          type='number'
+          placeholder='Quantity'
+          name='quantity'
+          value={quantity}
+          onChange={(e) => onChange(e)}
+          required
+        />
+        <h2>Description</h2>
+        <input
+          className='register-input'
+          type='text'
+          placeholder='Description'
+          name='description'
+          value={description}
+          onChange={(e) => onChange(e)}
+          required
+        />
+        <h2>Image</h2>
+        <input
+          className='register-input'
+          type='text'
+          placeholder='Image'
+          name='image'
+          value={image}
+          onChange={(e) => onChange(e)}
+          required
+        />
+        <button className='btn register-btn'>create</button>
+      </form>
+    </div>
+  );
+};
+
+EditProduct.propTypes = {
+  editProduct: PropTypes.func.isRequired,
+  admin: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  admin: state.admin,
+});
+
+export default connect(mapStateToProps, { editProduct })(EditProduct);
--- frontend/src/components/admin/ProductInfo.js
+++ frontend/src/components/admin/ProductInfo.js
@@ -3,6 +3,7 @@ import { Link, useParams } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { getProductById } from "../../actions/admin";
+import Loader from "../layouts/Loader";
 
 const ProductInfo = ({ getProductById, admin }) => {
   // GEt user by id
@@ -19,7 +20,27 @@ const ProductInfo = ({ getProductById, admin }) => {
       <Link to='/admin/productlist'>
         <button className='back-btn btn'>Go Back</button>
       </Link>
-      <h1>{admin.productDetails.name}</h1>
+      <h1>PRODUCT DETAILS</h1>
+      {!admin.loading ? (
+        <div>
+          <h2>Name:</h2>
+          <h2>{admin.productDetails.name}</h2>
+          <h2>Price:</h2>
+          <h2>{admin.productDetails.price}</h2>
+          <h2>Description:</h2>
+          <h2>{admin.productDetails.description}</h2>
+          <h2>Quantity:</h2>
+          <h2>{admin.productDetails.quantity}</h2>
+          <h2>Image:</h2>
+          <h2>{admin.productDetails.image} </h2>
+          <Link to='/admin/product/edit'>
+            <button className='btn primary-btn'>EDIT</button>
+          </Link>
+          <button className='btn primary-btn'>DELETE</button>
+        </div>
+      ) : (
+        <Loader />
+      )}
     </div>
   );
 };
