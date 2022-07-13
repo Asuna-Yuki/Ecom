8eea2e77b3eb55bcd64bef6911f0e5de5cdac819
Asuna-Yuki <rahulraj.sagg99@gmail.com>
2022-07-13 17:50:03 +0530

    added icon link and updated css

--- frontend/src/components/layouts/CartCard.js
+++ frontend/src/components/layouts/CartCard.js
@@ -10,13 +10,13 @@ const CartCard = ({ cartItem, removeItemInCart }) => {
     setData({ ...data, [e.target.name]: e.target.value });
   };
   return (
-    <div className='cart'>
+    <div className='cart-card'>
       <div className='cart-card-img'>
         <img className='image' src={cartItem.image} alt=''></img>
       </div>
       <div className='cart-card-des'>
         <h1>{cartItem.name}</h1>
-        <p>{cartItem.price}</p>
+        <p>₹{cartItem.price}</p>
         <div className='cart-card-des-menu'>
           <input
             type='number'
@@ -30,7 +30,7 @@ const CartCard = ({ cartItem, removeItemInCart }) => {
           <a href='#!' onClick={() => removeItemInCart(cartItem._id)}>
             Remove
           </a>
-        </div>
+        </div>{" "}
       </div>
     </div>
   );
